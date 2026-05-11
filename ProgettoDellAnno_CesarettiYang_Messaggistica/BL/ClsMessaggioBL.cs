4fb1bab6a745912b3c7a30e96a3acacb96ada93e
yang <st11318@iismarconipieralisi.it>
2026-05-11 12:40:45 +0200

    commento

---
+++ ProgettoDellAnno_CesarettiYang_Messaggistica/BL/ClsMessaggioBL.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace Messaggistica.BL
+{
+    class ClsMessaggioBL
+    {
+        #region ATTRIBUTI
+        private MySqlConnection _conn = null;
+        #endregion
+
+        #region COSTRUTTORI
+        public ClsMessaggioBL(MySqlConnection cn)
+        {
+            _conn = cn;
+        }
+        #endregion
+        #region METODI
+        #region CREARE
+        internal static long Create(ref MySqlConnection conn, ClsMessaggio messaggio, out string errore)
+        {
+            long ID = 0;
+            errore = String.Empty;
+
+            try
+            {
+                //apro la connessione
+                conn.Open();
+
+                //creo la query
+                string sql = "INSERT INTO messaggi (testo, data, mittenteID, destintarioID ) VALUES (@testo, @data, @mittenteID, @destintarioID)";
+
+                //creo l'oggetto command
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+                //assegno i valori
+                cmd.Parameters.AddWithValue("@testo", messaggio.Testo);
+                cmd.Parameters.AddWithValue("@data", messaggio.Data);
+                cmd.Parameters.AddWithValue("@mittenteID", messaggio.MittenteID);
+                cmd.Parameters.AddWithValue("@destintarioID", messaggio.DestintarioID);
+
+                //eseguo il comando
+                int numRec = cmd.ExecuteNonQuery();
+                if (numRec == 1)
+                    ID = cmd.LastInsertedId; //ottengo l'id automaticamente
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                errore = ex.Message;
+            }
+
+            return ID;
+        }
+        #endregion
+
+        #endregion
+    }
+}
--- ProgettoDellAnno_CesarettiYang_Messaggistica/DMO/ClsMessaggio.cs
+++ ProgettoDellAnno_CesarettiYang_Messaggistica/DMO/ClsMessaggio.cs
@@ -12,36 +12,32 @@ namespace Messaggistica
         private DateTime data;
         private bool eliminatoDaMittente;
         private bool eliminatoDaDestinatario;
+        private long mittenteID;
+        private long destintarioID;
+
 
 
         public string Testo { get => testo; set => testo = value; }
         public DateTime Data { get => data; set => data = value; }
         public bool EliminatoDaMittente { get => eliminatoDaMittente; set => eliminatoDaMittente = value; }
         public bool EliminatoDaDestinatario { get => eliminatoDaDestinatario; set => eliminatoDaDestinatario = value; }
+        public long DestintarioID { get => destintarioID; set => destintarioID = value; }
+        public long MittenteID { get => mittenteID; set => mittenteID = value; }
 
-        // --- COSTRUTTORI ---
+        // --- COSTRUTTORE ---
 
-        // 1. Costruttore Vuoto (Default)
         public ClsMessaggio()
         {
-            this.data = DateTime.Now; // Prende l'ora esatta del PC
-            this.eliminatoDaMittente = false;
-            this.eliminatoDaDestinatario = false;
-        }
 
-        // 2. Costruttore con il Testo (Il più usato per inviare)
-        public ClsMessaggio(string testo) : this()
-        {
-            this.testo = testo;
         }
-
-        // 3. Costruttore Completo (Utile se devi ricaricare messaggi vecchi dal DB)
-        public ClsMessaggio(string testo, DateTime data, bool eliminatoDaMittente, bool eliminatoDaDestinatario)
+        public ClsMessaggio(string testo, DateTime data, bool eliminatoDaMittente, bool eliminatoDaDestinatario, long mittenteID, long destintarioID)
         {
             this.testo = testo;
             this.data = data;
             this.eliminatoDaMittente = eliminatoDaMittente;
             this.eliminatoDaDestinatario = eliminatoDaDestinatario;
+            this.mittenteID = mittenteID;
+            this.destintarioID = destintarioID;
         }
     }
 }
